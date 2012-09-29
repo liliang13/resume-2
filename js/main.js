22fc81dec583d3c93ca3eb95715c8ec2fed3de2b
Letian Zhang <zlthooray@gmail.com>
2012-09-29 23:20:59 +0800

    two languages in topbar, and improve compatibility

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
 (function($){
+    String.prototype.format = function() {
+        var s = this,
+            i = arguments.length;
+
+        while (i--) {
+            s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);
+        }
+        return s;
+    };
+
     $.fn.Resume = function(options) {
+
+        var getLanguageFromHash = function() {
+            var hash = window.location.hash;
+            if (hash && $.inArray(hash, ['#zh', '#en']) >= 0)
+                return hash.substr(1);
+            else
+                return 'zh';
+        };
+
         var settings = $.extend({
             'source'          : $("#template").html(),
+            'topbar'          : $("#topbar"),
             'loadingBar'      : $("#loadingbar-wrapper"),
-            'defaultLanguage' : window.location.hash ? window.location.hash.substr(1) : 'zh'
+            'defaultLanguage' : getLanguageFromHash()
         }, options),
             container = this;
 
@@ -11,6 +31,27 @@
             container.hide();
             settings.loadingBar.show();
             $.getJSON(language + '.json', function(data) {
+                // set title
+                document.title = data.document_title;
+
+                // render topbar
+                var topbar_html_wrap = '<p>{0} <a class="source" href="https://github.com/clippit/resume" target="_blank">{1}</a></p>',
+                    topbar_html = topbar_html_wrap.format(
+                        data.language_tip.format(
+                            '<a class="select select-zh" href="#zh">中文</a>',
+                            '<a class="select select-en" href="#en">English</a>',
+                            '<a href="">{0}</a>'.format(data.download_pdf)
+                        ),
+                        data.view_source
+                    );
+                settings.topbar.html(topbar_html);
+                // for browser without support for hashchange event(IE7), simply refresh the page
+                if (!Modernizr.hashchange) {
+                    $('#topbar a.select').on('click', function() {
+                        window.location.reload();
+                    });
+                }
+
                 // render template
                 var result = $.mustache(settings.source, data);
                 container.html(result);
@@ -20,19 +61,20 @@
                         email = decodeURIComponent($e.data('email'));
                     $e.attr("href", "mailto:" + email).text(email);
                 });
+
                 // show result
                 settings.loadingBar.hide();
                 container.show();
-                $('.topbar a').removeClass('active').filter('.select-' + language).addClass('active');
+                $('#topbar a.select').removeClass('active').filter('.select-' + language).addClass('active');
             });
         };
 
         if (Modernizr.hashchange) {
             $(window).on('hashchange', function() {
-                var language = window.location.hash.substr(1);
-                loadResume(language);
+                loadResume(getLanguageFromHash());
             });
         }
+
         loadResume(settings.defaultLanguage);
         return this;
     };
